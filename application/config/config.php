449f01f956d20024d147854767c9505d2e01beac
root <root@ip-172-31-26-213.ap-northeast-2.compute.internal>
2019-04-13 09:15:57 +0000

    Killed index.php and Accomplished server setting

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,11 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost';
+$config['base_url'] = 'http://ec2-15-164-100-60.ap-northeast-2.compute.amazonaws.com/';
+// $config['base_url'] = ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == "on") ? "https" : "http");
+// $config['base_url'] .= "://" . $_SERVER['HTTP_HOST'];
+// $config['base_url'] .= str_replace(basename($_SERVER['SCRIPT_NAME']), "", $_SERVER['SCRIPT_NAME']);
+
 
 /*
 |--------------------------------------------------------------------------
